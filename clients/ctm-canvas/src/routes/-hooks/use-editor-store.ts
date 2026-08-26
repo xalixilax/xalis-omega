da061e5abe58b7fd0e5e5252fa9533c361f2f073
xalixilax <xalixilax@gmail.com>
2026-08-25 21:56:26 -0400

    chore: remove old project

--- clients/ctm-canvas/src/routes/-hooks/use-editor-store.ts
+++
@@ -1,6 +0,0 @@
-import { useStore } from "@tanstack/react-store"
-import { editorStore, type EditorState } from "#/routes/-lib/store"
-
-export function useEditorState(): EditorState {
-  return useStore(editorStore, (s) => s)
-}
